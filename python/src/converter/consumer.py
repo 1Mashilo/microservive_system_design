3df5f66654af7a905880024273dc045c76ee0135
1Mashilo <christophermashilo@gmail.com>
2024-02-05 17:46:33 +0200

    configured rabbitmq connection

--- python/src/converter/consumer.py
+++ python/src/converter/consumer.py
@@ -1,10 +1,10 @@
 import pika
 import sys
 import os
-import time
 from pymongo import MongoClient
 from gridfs import GridFS
 from convert import to_mp3
+from dotenv import load_dotenv
 
 def main():
     # Connect to MongoDB
@@ -15,33 +15,45 @@ def main():
     # Set up GridFS for storing files
     fs_videos = GridFS(db_videos)
     fs_mp3s = GridFS(db_mp3s)
+   
+    #rabbitmq connection config
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="rabbitmq") # our service name is rabbitmq
+    )
+    channel = connection.channel()
 
-# Get AMQP URL from environment variable
-amqp_url = os.getenv("AMQP_URL")
 
-# Configure RabbitMQ connection
-params = pika.URLParameters(amqp_url)
-connection = pika.BlockingConnection(params)
-channel = connection.channel()
+    def callback(ch, method, properties, body):
+        # Call function to convert video to mp3
+        err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
+        if err:
+            # If error, reject message and request re-delivery
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            # If successful, acknowledge message
+            ch.basic_ack(delivery_tag=method.delivery_tag)
 
-def callback(ch, method, properties, body):
-    # Call function to convert video to mp3
-    err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
-    if err:
-        # If error, reject message and request re-delivery
-        ch.basic_nack(delivery_tag=method.delivery_tag)
-    else:
-        # If successful, acknowledge message
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+    # Start consuming messages from the video queue
+    channel.basic_consume(
+        queue=os.environ.get("VIDEO_QUEUE"),
+        on_message_callback=callback
+    )
 
-# Start consuming messages from the video queue
-channel.basic_consume(
-    queue=os.environ.get("VIDEO_QUEUE"),
-    on_message_callback=callback
-)
+    # Print a message indicating that the program has started consuming messages
+    print("Waiting for messages. To exit, press CTRL+C")
 
-# Print a message indicating that the program has started consuming messages
-print("waiting for messages. To exit press CTRL+C")
+    # Keep consuming messages until program is terminated
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
 
-# Keep consuming messages until program is terminated
-channel.start_consuming()
+if __name__ == "__main__":
+    main()
+
+        
+

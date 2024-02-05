627d2e32a6c3701919e372b93d00593767448676
1Mashilo <christophermashilo@gmail.com>
2024-02-05 20:26:54 +0200

    updated the download API endpoint and added documentations

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -1,68 +1,102 @@
 import os
+import gridfs
 import pika
 import json
-from flask import Flask, request
-from flask_pymongo import PyMongo 
+from flask import Flask, request, send_file
+from flask_pymongo import PyMongo
 from auth import validate
-from auth_svc import access 
+from auth_svc import access
 from storage import util
-from gridfs import GridFS
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
+from bson.objectid import ObjectId
 
 # Initialize Flask app
 server = Flask(__name__)
 
-# Configure MongoDB connection URI
-server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
+# Connect to MongoDB for videos and mp3s
+mongo_video = PyMongo(server, uri="mongodb://host.minikube.internal:27017/videos")
+mongo_mp3 = PyMongo(server, uri="mongodb://host.minikube.internal:27017/mp3s")
 
-# Initialize PyMongo to connect to MongoDB
-mongo = PyMongo(server)
+# Initialize GridFS for storing videos and mp3s
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
-# Initialize GridFS for file storage
-fs = GridFS(mongo.db)
-
-# Get AMQP URL from environment variable
-amqp_url = os.getenv("AMQP_URL")
-
-# Configure RabbitMQ connection
-params = pika.URLParameters(amqp_url)
-connection = pika.BlockingConnection(params)
+# Connect to RabbitMQ for messaging
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
 
-# Route for user login
 @server.route("/login", methods=["POST"])
 def login():
+    """
+    Endpoint for user login authentication.
+
+    Returns:
+        str: Token if successful, error message otherwise.
+    """
     token, err = access.login(request)
+
     if not err:
         return token
     else:
-        return err, 401
+        return err
 
-# Route for file upload
 @server.route("/upload", methods=["POST"])
 def upload():
-    # Validate user token
+    """
+    Endpoint for uploading files.
+
+    Returns:
+        str: Success message if upload is successful, error message otherwise.
+    """
     access, err = validate.token(request)
-    access, err = json.loads(access)
+
+    if err:
+        return err
+
+    access = json.loads(access)
 
     if access["admin"]:
         if len(request.files) != 1:
             return "Exactly 1 file required", 400
 
-    for _, f in request.files.items():
-        err = util.upload(f, fs, channel, access)
-        if err:
-            return err, 401
+        for _, f in request.files.items():
+            err = util.upload(f, fs_videos, channel, access)
 
-    return "Success!", 200
+            if err:
+                return err
 
-# Route for file download - Needs implementation
+        return "Success!", 200
+    else:
+        return "Not authorized", 401
+
 @server.route("/download", methods=["GET"])
 def download():
-    pass
+    """
+    Endpoint for downloading files.
+
+    Returns:
+        File: Downloaded file if successful, error message otherwise.
+    """
+    access, err = validate.token(request)
+
+    if err:
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "fid is required", 400
+
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f"{fid_string}.mp3")
+        except Exception as err:
+            print(err)
+            return "Internal server error", 500
+
+    return "Not authorized", 401
 
 if __name__ == "__main__":
-    server.run(debug=True)
+    server.run(host="0.0.0.0", port=8080)
